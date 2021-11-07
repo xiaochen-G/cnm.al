9a57c73982994f5140a7a057492ea8ac118f672b
xiaochen-G <93259271+xiaochen-G@users.noreply.github.com>
2021-11-07 10:36:12 +0800

    Add files via upload

---
+++ js/cnm.sb_judge.js
@@ -0,0 +1,10 @@
+//页面展示切换
+$(document).ready(function(){
+	if(window.screen.width > window.screen.height){	
+		$("#pc").show();
+		$("#mp").hide();
+	}else{
+		$("#pc").hide();
+		$("#mp").show();
+	}
+})
